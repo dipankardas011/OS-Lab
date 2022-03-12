88db03620744c12e99592403c1034bd5f1cf5e4c
DipankarDas <dipsonu10@hotmail.com>
2022-03-12 23:35:33 +0530

    Added debug for the Q1ii.c

--- Lab05/Q1ii.c
+++ Lab05/Q1ii.c
@@ -6,10 +6,9 @@
 #include <sys/wait.h>
 
 int main(int argc, char **argv) {
-  
-  for (int i = 0; i < 4; i++)
-  {
-    printf("P%d\n",i+1);
+  printf("NAME\tPID\tPPID\n");
+  for (int i = 0; i < 4; i++) {
+    printf("P%d\t%d\t%d\n",i+1, getpid(), getppid());
     pid_t t = fork();
     assert(t >= 0);
     if (t != 0)
