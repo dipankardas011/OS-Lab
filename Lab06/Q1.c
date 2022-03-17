bbc893e32ff3fc08436be3850aad073dddd75f7a
Ubuntu <azureuser@OSLab.ptbgeglkbz0enfzn3cpcoxsbdh.bx.internal.cloudapp.net>
2022-03-17 07:02:55 +0000

    Done Q1 Lab06

---
+++ Lab06/Q1.c
@@ -0,0 +1,32 @@
+/**
+ * parent process has to wait till the complete of all the child process so that no child process will become orphan
+ */
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <assert.h>
+
+int main(int argc, char *argv[]) {
+	int N;
+	printf("Enter the number of child processes to be created: ");
+	scanf("%d",&N);
+	pid_t t = 0;
+
+	for (int i = 0; i<N;i++) {
+		t = fork();
+		assert(t >= 0);
+		if (t == 0) {
+			printf("Child Process pid: %d\tppid: %d\n",getpid(), getppid());
+			exit(0);
+		} else {
+			printf("Parent Process pid: %d\tppid: %d\n",getpid(), getppid());
+			wait(NULL);
+		}
+	}
+	//wait(NULL);
+	remove(argv[0]);
+	return 0;
+}
