e2124f832a436139a127f4c5db67f96eaee73a50
Dipankar Das <dipsonu10@hotmail.com>
2022-04-28 13:13:45 +0530

    Done Q1 Lab10

---
+++ Lab10/proc.h
@@ -0,0 +1,25 @@
+#ifndef STDLIB_H
+#include <stdlib.h>
+#endif
+
+enum state {
+  RUNNING,
+  RUNNABLE,
+  // WAITING,
+  TERMINATED,
+  EMBRYO
+};
+
+
+struct proc {
+  int pid;
+  int arrTime;
+  int burstTime;
+  enum state currState;
+  int initStartTime;
+  int finalEndTime;
+  int priority;
+};
+
+struct proc* Rqueue = NULL;
+int *tempStoreBT = NULL;
